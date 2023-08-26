069ea25b113b299929e8cfd411fff411e5f72af7
NnR2D2 <107245457+NnR2D2@users.noreply.github.com>
2023-08-26 17:56:58 +0600

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,4 +1,4 @@
-// Write a program to manage Food order in a restaurant. In a restaurant, the food order
+// a program to manage Food order in a restaurant. In a restaurant, the food order
 // contains food number, size, and quantity. The order is served in the form of first come
 // first serve basis. You program will display total number of orders received, served and
 // waiting at any point in time.
